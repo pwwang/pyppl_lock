cca0959fefafa43f61562d815e3579ff59f656fc
pwwang <pwwang@pwwang.com>
2020-05-24 18:44:12 -0500

    Fix proc not unlocked when pipeline done unexpectedly.

--- pyppl_lock.py
+++ pyppl_lock.py
@@ -5,7 +5,10 @@ from pyppl.logger import Logger
 
 __version__ = "0.0.5"
 
-logger = Logger(plugin='lock') # pylint: disable=invalid-name
+# pylint: disable=unused-argument
+
+LOGGER = Logger(plugin='lock')
+LAST_PROC = [None]
 
 @hookimpl
 def proc_init(proc):
@@ -17,17 +20,18 @@ def proc_init(proc):
 def proc_prerun(proc):
     """Try to access the lock"""
     lockfile = proc.workdir / 'proc.lock'
+    LAST_PROC[0] = proc
     lock = proc.props.lock_lock = filelock.SoftFileLock(lockfile)
     try:
         lock.acquire(timeout=3)
     except filelock.Timeout:
-        logger.warning(
+        LOGGER.warning(
             'Another instance of this process is running, waiting ...',
             proc=proc.id)
-        logger.warning('If not, remove the process lock file '
+        LOGGER.warning('If not, remove the process lock file '
                        '(or hit Ctrl-C) and try again:',
                        proc=proc.id)
-        logger.warning('- %s', lockfile, proc=proc.id)
+        LOGGER.warning('- %s', lockfile, proc=proc.id)
         try:
             lock.acquire()
         except KeyboardInterrupt:  # pragma: no cover
@@ -55,6 +59,12 @@ def job_build(job, status):
 
 
 @hookimpl
-def proc_postrun(proc, status):  # pylint: disable=unused-argument
+def proc_postrun(proc, status):
     """We should remove the lock file anyway"""
     _lock_release(proc)
+
+@hookimpl
+def pyppl_postrun(ppl):
+    """We should remove the lock file anyway when pipeline is done"""
+    if LAST_PROC[0]:
+        _lock_release(LAST_PROC[0])
